92ed20c253c49850e966f6ff06ee0a24ebf39b63
TheArRow513 <iaboode99@gmail.com>
2026-08-03 16:54:19 +0300

    created

---
+++ sysinfo.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+echo "===================================================="
+echo "			System Information"
+echo "===================================================="
+
+echo "Hostname: $(hostname)"
+echo "User: $(whoami)"
+echo "Date: $(date)"
+echo""
+
+echo "OS:"
+cat /etc/os-release | grep PRETTY_NAME
+
+echo ""
+
+echo "kernal:"
+uname -r
+
+echo "CPU"
+lscpu | grep "Mode Name" | cut -d ':' -f2
+
+echo""
+
+echo "Memory"
+free -h
+
+echo""
+
+echo "Disk:"
+df -h /
+
+echo""
+
+echo"Uptime:"
+uptime -p
+
+echo""
+
