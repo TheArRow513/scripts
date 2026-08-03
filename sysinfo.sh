7c2402446526b3f78d8b99a990da2a864a1b71f4
TheArRow513 <iaboode99@gmail.com>
2026-08-03 17:23:40 +0300

    improvment

--- sysinfo.sh
+++ sysinfo.sh
@@ -8,32 +8,71 @@ echo "User: $(whoami)"
 echo "Date: $(date)"
 echo ""
 
-echo "OS:"
-cat /etc/os-release | grep PRETTY_NAME
+echo "===================================================="
+echo "                  Operating System"
+echo "===================================================="
 
+
+if [ -f /etc/os-release ]; then
+	grep PRETTY_NAME /etc/os-realease | cut -d '"' -f2
+fi
+
+echo "kernal	: $(uname -r)"
 echo ""
 
-echo "kernal:"
-uname -r
+echo "===================================================="
+echo "                  CPU Information"
+echo "===================================================="
 
+echo "CPU	:$(lscpu | grep "Model name" | cut -d ':' -f2 | sed 's/^ *//')"
+echo "cores	:$(nproc)"
 echo ""
-echo "CPU:"
- lscpu | grep "Model name" | cut -d ':' -f2
 
-echo ""
+echo "===================================================="
+echo "                  Memory"
+echo "===================================================="
 
-echo "Memory"
 free -h
 
 echo ""
 
-echo "Disk:"
+echo "===================================================="
+echo "                  Storage"
+echo "===================================================="
+
 df -h /
 
 echo ""
 
-echo "Uptime:"
+echo "===================================================="
+echo "                  Network"
+echo "===================================================="
+
+INTERFACE=$(ip route | grep default | awk '{print $5}')
+echo "Interface: $INTERFACE"
+
+echo "Local IP : $(ip -r addr show $INTERFACE | grep inet | awk '{print $2}' | cut -d/ -f1)"
+
+echo "MAC addr : $(cat /sys/class/net/$INTERFACE/address)"
+
+echo ""
+
+echo "================================="
+echo "        UPTIME"
+echo "================================="
 uptime -p
 
-echo "===================================================="
+echo ""
+
+echo "================================="
+echo "        System Status"
+echo "================================="
+
+echo "Runing Processes: $(ps aux | wc -l)"
+echo "Logged users: $(who | wc -l)"
+
+echo ""
+echo "================================="
+echo "             DONE"
+echo "================================="
 
